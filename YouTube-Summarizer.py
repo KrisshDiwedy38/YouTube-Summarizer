6e950db8a056f7c42766fcd67254665886437376
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-02-26 22:20:28 +0530

    Updating

--- YouTube-Summarizer.py
+++ YouTube-Summarizer.py
@@ -1,5 +1,21 @@
-def main():
+import youtube_transcript_api
+
+def get_video_id(video_link):
    pass
+
+def get_video_transcript(video_id):
+   pass
+
+def get_video_summary(video_transcript):
+   pass
+
+def main():
+   video_link = input("Enter YouTube video link: ").strip()
+   video_id = get_video_id(video_link)
+   video_transcript = get_video_transcript(video_id)
+   video_sumamry = get_video_summary(video_transcript)
+   return video_sumamry
+
 
 if __name__ == "__main__":
    main()
