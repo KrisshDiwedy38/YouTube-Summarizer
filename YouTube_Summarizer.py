c0379ffa6803377fc6c7457a71c93d7c552fd560
Krissh Diwedy <krisshdiwedy38@gmail.com>
2025-02-27 10:13:15 +0530

    Adding Jupiter File

---
+++ YouTube_Summarizer.py
@@ -0,0 +1,24 @@
+import youtube_transcript_api
+
+def get_video_id(video_link):
+   pass
+
+def get_video_transcript(video_id):
+   pass
+
+def get_video_summary(video_transcript):
+   pass
+
+def main():
+   video_link = input("Enter YouTube video link: ").strip()
+   video_id = get_video_id(video_link)
+   video_transcript = get_video_transcript(video_id)
+   video_sumamry = get_video_summary(video_transcript)
+   return video_sumamry
+
+
+if __name__ == "__main__":
+   main()
+
+
+name = 'krissh'
